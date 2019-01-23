c686e5acaa6cfcb6319e5ddd555ca7c2f02ac416
D.S. Webster <dswebs.me@gmail.com>
2019-01-23 11:44:28 -0500

    Add the childtheme folder as a starting point for child theme development

---
+++ childtheme/functions.php
@@ -0,0 +1,10 @@
+<?php
+define( 'CHILD_THEME_NAME', 'Child Theme' );
+define( 'CHILD_THEME_URL', 'https://github.com/pressden/baseline' );
+define( 'CHILD_THEME_VERSION', '1.0.0' );
+
+/**********************************
+ * BEGIN THEME FUNCTIONS
+ *
+ * Theme specific customizations should be added below.
+ **********************************/
