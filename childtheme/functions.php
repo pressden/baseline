754334bec5338f42c272799c5ae58e3f456c28b1
D.S. Webster <dswebs.me@gmail.com>
2019-01-25 08:20:50 -0500

    Add PHP assets and baseline styles

--- childtheme/functions.php
+++ childtheme/functions.php
@@ -3,6 +3,8 @@ define( 'CHILD_THEME_NAME', 'Child Theme' );
 define( 'CHILD_THEME_URL', 'https://github.com/pressden/baseline' );
 define( 'CHILD_THEME_VERSION', '1.0.0' );
 
+require_once( 'baseline/assets/php/baseline.php');
+
 /**********************************
  * BEGIN THEME FUNCTIONS
  *
