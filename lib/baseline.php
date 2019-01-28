14b6d864dc34e44558d9f5b1dd8bc3f3e64df918
D.S. Webster <dswebs.me@gmail.com>
2019-01-27 23:07:47 -0500

    Move the secondary sidebar inside .content-sidebar-wrap

--- lib/baseline.php
+++ lib/baseline.php
@@ -15,3 +15,10 @@ function baseline_move_subnav() {
   remove_action( 'genesis_after_header', 'genesis_do_subnav' );
   add_action( 'genesis_header_right', 'genesis_do_subnav', 5 );
 }
+
+// move secondary sidebar inside .content-sidebar-wrap
+add_action( 'after_setup_theme', 'baseline_move_sidebar_alt' );
+function baseline_move_sidebar_alt() {
+  remove_action( 'genesis_after_content_sidebar_wrap', 'genesis_get_sidebar_alt' );
+  add_action( 'genesis_after_content', 'genesis_get_sidebar_alt' );
+}
