dd66c33c4082b74281a0588c6af55f7bf3b53c27
D.S. Webster <dswebs.me@gmail.com>
2019-02-22 08:28:14 -0500

    Update functions.php to include comments and Genesis initialization.

--- childtheme/functions.php
+++ childtheme/functions.php
@@ -1,8 +1,24 @@
 <?php
+/**
+ * Baseline
+ *
+ * This file adds functions to the child theme.
+ *
+ * @package Baseline
+ * @author  D.S. Webster
+ * @license GPL-2.0-or-later
+ * @link    https://github.com/pressden/baseline
+ */
+
+// Initialize Genesis.
+require_once get_template_directory() . '/lib/init.php';
+
+// Defines the child theme (do not remove).
 define( 'CHILD_THEME_NAME', 'Child Theme' );
 define( 'CHILD_THEME_URL', 'https://github.com/pressden/baseline' );
 define( 'CHILD_THEME_VERSION', '1.0.0' );
 
+// Sets up the Theme.
 require_once( 'baseline/lib/baseline.php');
 
 /**********************************
