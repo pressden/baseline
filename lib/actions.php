954e0ed4d07555527d9f0e93c510868e9ca05b40
D.S. Webster <dswebs.me@gmail.com>
2019-03-01 11:22:57 -0500

    Put the menus in the correct order

--- lib/actions.php
+++ lib/actions.php
@@ -6,8 +6,8 @@ add_action( 'genesis_site_title', 'the_custom_logo', 0 );
 
 // Repositions primary navigation menu.
 remove_action( 'genesis_after_header', 'genesis_do_nav' );
-add_action( 'genesis_header', 'genesis_do_nav', 12 );
+add_action( 'genesis_header', 'genesis_do_nav', 5 );
 
 // Repositions secondary navigation menu.
 remove_action( 'genesis_after_header', 'genesis_do_subnav' );
-add_action( 'genesis_header', 'genesis_do_subnav', 5 );
+add_action( 'genesis_header', 'genesis_do_subnav', 12 );
