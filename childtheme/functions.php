cf5345776e2edc765a7e81f695b75f4d8fb010af
D.S. Webster <dswebs.me@gmail.com>
2019-03-11 12:09:06 -0400

    Add color helpers to the starter functions.php file

--- childtheme/functions.php
+++ childtheme/functions.php
@@ -26,3 +26,13 @@ require_once( 'baseline/lib/baseline.php');
  *
  * Theme specific customizations should be added below.
  **********************************/
+
+// Gets default link color.
+function childtheme_get_default_link_color() {
+	return '#00b9eb';
+}
+
+// Gets default accent color.
+function childtheme_get_default_accent_color() {
+	return '#00b9eb';
+}
