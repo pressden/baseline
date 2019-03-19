bf48a9983161aa64a3a0f2e1ce11423e1dede9fd
D.S. Webster <dswebs.me@gmail.com>
2019-03-19 15:07:24 -0400

    Uncomment a working JS file

--- childtheme/js/webpack.js
+++ childtheme/js/webpack.js
@@ -1,7 +1,7 @@
 // JS
 require('../baseline/assets/js/baseline');
 require('../baseline/assets/js/responsive-menus.js');
-//require('../baseline/assets/js/sticky-header.js');
+require('../baseline/assets/js/sticky-header.js');
 
 // CSS && SCSS
 require('../scss/theme-header.scss');                // used by WordPress to identify the theme
