5f0eb21c79fb27566942d61b102ed8c9ff24737d
D.S. Webster <dswebs.me@gmail.com>
2019-03-19 12:39:21 -0400

    Initialize last to ensure all actions, helpers, filters, etc. are preloaded

--- lib/baseline.php
+++ lib/baseline.php
@@ -5,8 +5,8 @@
  * This file includes all files required by baseline.
  **********************************/
 // load core files
-require_once( 'initialize.php' );
 require_once( 'helpers.php' );
 require_once( 'actions.php' );
 require_once( 'filters.php' );
 require_once( 'frontend.php' );
+require_once( 'initialize.php' );
